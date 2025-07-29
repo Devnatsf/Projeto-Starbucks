79d65397d00c55b8731fd372f0b06a265bad8398
Devnatsf <natysouza024@gmail.com>
2025-07-28 21:53:35 -0300

    Projeto Starbucks

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const imagem = document.querySelector(".img1")
+const circulo= document.querySelector(".circulo")
+
+function trocarImagem(endereco){
+    imagem.src= endereco
+    }
+    
+function trocarCor(cor){
+    circulo.style.background = cor
+
+}
+
